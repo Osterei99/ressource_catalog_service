af0bc35b5b98266aa1835fb9962ac1635dc9bece
Kai Brokmann <133008349+Osterei99@users.noreply.github.com>
2025-08-04 12:30:36 +0200

    RC-002 Implemented Error

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import http from 'http';
+import { readFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
+const app = express();
+
+const port = 5002;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const data_file = path.join(__dirname, "data", 'resources.json')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Welcome to Resource Catalog');
+});
+
+app.get('/resources', (req, res) => {
+   try {
+        const data = readFileSync(data_file, 'utf8');
+        const resources = JSON.parse(data);
+        res.json(resources);
+    } catch (error) {
+        res.status(500).json({ error: 'Interner Serverfehler beim Laden der Daten' });
+    }
+    
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Server is running at http://localhost:${port}`);
+});
